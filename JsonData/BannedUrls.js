52627643e2d60ed491e4708ec23aa3a2e68a1c07
ukdevelopers007 <113181134+ukdevelopers007@users.noreply.github.com>
2025-01-16 19:39:34 +0545

    dsaf

--- JsonData/BannedUrls.js
+++ JsonData/BannedUrls.js
@@ -1,13 +1,13 @@
 // this is to remove urls that contains dmca notice in google search consile
 export const BannedUrls = [
-  "https://chutlunds.com/video/7pjtq*popsytopsy+01",
-  "https://chutlunds.com/video/7pojn*popsytopsy+03",
-  "https://chutlunds.com/video/7pojx*popsytopsy+02",
-  "https://chutlunds.com/video/7tmeb*popsytopsy+04",
-  "https://chutlunds.com/video/7uyeq*popsytopsy+05",
-  "https://chutlunds.com/video/80ch7*popsytopsy+06",
-  "https://chutlunds.com/video/80ch8*popsytopsy+07",
-  "https://chutlunds.com/video/85ycs*popsytopsy+big+tits+with+oil",
-  "https://chutlunds.com/video/8z7ui*popsytopsy",
-  "https://chutlunds.com/search/janet%20mason/page/0"
+  "https://milfymadness.com/video/7pjtq*popsytopsy+01",
+  "https://milfymadness.com/video/7pojn*popsytopsy+03",
+  "https://milfymadness.com/video/7pojx*popsytopsy+02",
+  "https://milfymadness.com/video/7tmeb*popsytopsy+04",
+  "https://milfymadness.com/video/7uyeq*popsytopsy+05",
+  "https://milfymadness.com/video/80ch7*popsytopsy+06",
+  "https://milfymadness.com/video/80ch8*popsytopsy+07",
+  "https://milfymadness.com/video/85ycs*popsytopsy+big+tits+with+oil",
+  "https://milfymadness.com/video/8z7ui*popsytopsy",
+  "https://milfymadness.com/search/janet%20mason/page/0"
 ];
