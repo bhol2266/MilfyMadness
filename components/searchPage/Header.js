eab42171ce5fa76d521f94a980136c4432a93b0b
ukdevelopers007 <113181134+ukdevelopers007@users.noreply.github.com>
2025-04-10 22:59:00 +0545

    asdfdsasadfasdf

--- components/searchPage/Header.js
+++ components/searchPage/Header.js
@@ -35,14 +35,13 @@ export default function Header({ keyword, pageNumber, filteredObjsArrayProps }) 
     useEffect(() => {
         const href = window.location.href;
 
-        if (href === window.location.origin + "/" || href.includes("/query")) {
+        if (href === window.location.origin + "/") {
             setcurrentPage("Homepage");
         } else if (href.includes('/search/')) {
             setcurrentPage("searchPage");
         } else {
             setcurrentPage("categoryPage");
-        }
-    }, [])
+        }    }, [])
 
 
 
@@ -162,8 +161,6 @@ export default function Header({ keyword, pageNumber, filteredObjsArrayProps }) 
     ]
 
     const clickHandler = (query) => {
-        console.log(currentPage);
-
         setSpinner(true)
         var queryObj = {}
         //if this Header component is of search page or category page
@@ -191,7 +188,6 @@ export default function Header({ keyword, pageNumber, filteredObjsArrayProps }) 
         }
 
 
-
         if (currentPage === 'searchPage') {
             Router.push({
                 pathname: `/search/query/`,
@@ -199,7 +195,7 @@ export default function Header({ keyword, pageNumber, filteredObjsArrayProps }) 
             });
         } else if (currentPage === 'Homepage') {
             Router.push({
-                pathname: `/query/`,
+                pathname: `/query/`, // or whatever path you want for the homepage query
                 query: queryObj
             });
         } else {
@@ -284,10 +280,10 @@ export default function Header({ keyword, pageNumber, filteredObjsArrayProps }) 
                 <div className='flex items-center flex-wrap justify-start space-x-1 md:space-x-2 mb-2 md:mb-0 mr-1'>
                     {Final_filteredArray.map(item => {
                         return (
-                            <div key={item} onClick={() => { removefilter(item) }} className=' text-xs md:text-sm hover:bg-red-600 cursor-pointer bg-theme_green rounded-lg px-2 py-1 flex items-center space-x-1'>
-                                <p className=' text-white font-inter ' >{item}</p>
-                                <XCircleIcon className='h-4 md:h-6 text-white' />
-                            </div>
+                            <div key={item} onClick={() => { removefilter(item) }} className='mr-1 md:mr-2 text-xs md:text-sm hover:bg-red-800 cursor-pointer bg-red-500 rounded-lg px-2 py-1 flex items-center space-x-1 '>
+                            <p className=' text-white font-inter ' >{item}</p>
+                            <XCircleIcon className='h-4 md:h-6 text-white' />
+                        </div>
                         )
                     })}
                 </div>
